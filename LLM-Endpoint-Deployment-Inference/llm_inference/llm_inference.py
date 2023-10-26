41afb74bf4d4d5890936a62f30944a5582b803d2
Akash Mathur <63149422+akashmathur-2212@users.noreply.github.com>
2023-10-26 19:10:11 +0530

    py updated

---
+++ LLM-Endpoint-Deployment-Inference/llm_inference/llm_inference.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+# grab environment variables
+ENDPOINT_NAME = 'llm-endpoint-name'
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='application/json',
+                                       Accept='application/json',
+                                       Body=event['body'])
+
+    response_payload = response['Body'].read().decode('utf-8')
+    result = json.loads(response_payload)
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps(result)
+    }
+ 
