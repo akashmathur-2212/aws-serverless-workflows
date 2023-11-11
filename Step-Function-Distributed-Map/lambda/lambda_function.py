077b77a2d2d6d5416a31e839ccd333d3a1474da0
Akash Mathur <63149422+akashmathur-2212@users.noreply.github.com>
2023-11-11 16:45:21 +0530

    distributed-map

---
+++ Step-Function-Distributed-Map/lambda/lambda_function.py
@@ -0,0 +1,20 @@
+# Lambda Function: post-pocessing Function
+import json
+import re
+
+def lambda_handler(event, context):
+    # Assuming event contains the row data
+    row_data = event['column_name']
+    
+    # Perform your post-processing operations on row_data
+    cleaned_text = re.sub(r'[^A-Za-z\s]', '', row_data)
+
+    # Remove extra whitespaces
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+    
+    processed_result = f"Processed data: {cleaned_text}"
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(processed_result)
+    }
