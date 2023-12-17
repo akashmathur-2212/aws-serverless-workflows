c43d907bb7e48818a7294e2edec6ac9cb6e800ac
Akash Mathur <63149422+akashmathur-2212@users.noreply.github.com>
2023-12-17 17:02:24 +0530

    llm_inference.py

--- LLM-Endpoint-Deployment-Inference/llm_inference/llm_inference.py
+++ LLM-Endpoint-Deployment-Inference/llm_inference/llm_inference.py
@@ -2,7 +2,7 @@ import json
 import boto3
 
 # grab environment variables
-ENDPOINT_NAME = 'llm-endpoint-name'  
+ENDPOINT_NAME = 'llm-endpoint-name'   
 runtime= boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
